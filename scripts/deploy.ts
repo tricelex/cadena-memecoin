49d9c515b44e762385ce49fbcb7c91ffb68ff3c6
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-04-11 16:36:49 +0100

    Initial Commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const [owner] = await ethers.getSigners();
+
+  // We get the contract to deploy
+  const memeCoinFactory = await ethers.getContractFactory("MemeCoin");
+  const jokeCoin = await memeCoinFactory.deploy();
+
+  await jokeCoin.deployed();
+
+  console.log("MemeCoin Contract deployed to:", jokeCoin.address);
+  console.log("MemeCoin Contract owner address:", owner.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
